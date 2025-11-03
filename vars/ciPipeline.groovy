10256c72e007ec8357091d4a764ff28842f26807
Thirdeyecreative <thirdeyegfx@gmail.com>
2025-11-03 20:35:44 +0530

    Update ciPipeline.groovy if issue

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,44 +1,45 @@
-// This is the universal script for both Bitbucket and GitHub
+// This is the universal script for both Bitbucket and GitHub (Final Version)
 def call(Map config = [:]) {
     pipeline {
         agent { label 'debian-master' }
 
-        // *** THIS IS THE NEW, CRITICAL FIX ***
+        // This IS still needed to stop the broken default checkout
         options {
-            // This stops Jenkins from running the implicit 'Checkout SCM'
-            // that was using the wrong credential (the API one) and hanging.
             skipDefaultCheckout true
         }
-        // ************************************
 
         environment {
             SONAR_AUTH_TOKEN = credentials('sonar-global-token')
             // Credential ID for Bitbucket Git checkout
-            // GitHub builds will just ignore this.
             BITBUCKET_GIT_CREDS_ID = 'devops.thirdeyecreative' 
         }
 
         stages {
             
-            stage('Checkout Code') { // Our explicit checkout will run instead
+            stage('Checkout Code') {
                 steps {
                     script {
-                        if (env.BITBUCKET_GIT_URL) {
+                        // This is the new, more robust check.
+                        // It checks the class of the SCM object itself.
+                        if (scm.getClass().getName().contains('BitbucketSCMSource')) {
+                            
                             // --- IF IT'S A BITBUCKET BUILD ---
-                            echo "Bitbucket build detected. Running explicit checkout..."
-                            checkout([
-                                $class: 'GitSCM',
-                                // The plugin provides these variables for us
-                                userRemoteConfigs: [[
-                                    url: env.BITBUCKET_GIT_URL, 
-                                    credentialsId: BITBUCKET_GIT_CREDS_ID
-                                ]],
-                                branches: [[name: env.BRANCH_NAME]] 
-                            ])
+                            echo "Bitbucket SCM detected. Overriding checkout credentials."
+                            
+                            // 1. Get the SCM definition from the job
+                            def bitbucketScm = scm
+                            
+                            // 2. FORCE the credentialId to be our Git credential
+                            // This is the most important part.
+                            bitbucketScm.userRemoteConfigs[0].credentialsId = BITBUCKET_GIT_CREDS_ID
+                            
+                            // 3. Now, call checkout with the *modified* scm object
+                            checkout bitbucketScm
+                            
                         } else {
                             // --- IF IT'S A GITHUB BUILD ---
-                            echo "GitHub build detected. Running standard checkout scm."
-                            // This will run for your GitHub builds
+                            echo "GitHub SCM detected. Running standard checkout scm."
+                            // This will run for your GitHub builds and will work correctly.
                             checkout scm
                         }
                     }
@@ -52,6 +53,7 @@ def call(Map config = [:]) {
                             if (env.CHANGE_ID) {
                                 echo "INFO: Pull Request build detected. Running SonarQube PR analysis."
                                 
+                                // This logic correctly finds the PR branch name for BOTH platforms
                                 def prBranch = env.CHANGE_BRANCH ?: env.BRANCH_NAME
 
                                 sh """
